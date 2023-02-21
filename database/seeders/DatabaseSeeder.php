d4bc25dca669877a51e5e024f1a381c5e01e1474
yulips <anayuliputri@gmail.com>
2023-02-21 14:22:44 +0700

    frotend pages, view, & controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call ([
-            //TypeUserSeeder::class,
+            TypeUserSeeder::class,
             SpecialistSeeder::class,
             ConsultationSeeder::class,
             ConfigPaymentSeeder::class,
