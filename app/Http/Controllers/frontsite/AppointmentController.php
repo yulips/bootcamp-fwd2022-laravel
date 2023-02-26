d89e041a1c4b69a54dcf3649cdeb848346c4ee5d
yulips <anayuliputri@gmail.com>
2023-02-26 13:18:28 +0700

    add controller for frontsite 50%

---
+++ app/Http/Controllers/frontsite/AppointmentController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\frontsite;
+
+// use library here
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response; //wajib 
+use Illuminate\Suppport\Facades\Storage;
+use Illuminate\Http\Request;
+
+//  use everything here 
+// use gate
+use Auth;
+
+//use models
+use App\Models\User;
+use App\Models\Operational\Doctor;
+use App\Models\MaterData\Consultation;
+
+// thirdparty packages
+
+class AppointmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function _construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        return view('pages.frontsite.appointment.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
